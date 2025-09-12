ba768f40b1f179689b11e821735d065a3062c147
Qiiqqq <snrifqii@gmail.com>
2025-09-12 09:54:45 +0800

    Perbaikan pada Layout Login dan penambahan button login google di registerwizard

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -46,7 +46,7 @@ export default function Login({ status, canResetPassword }) {
                                     }}
                                 >
                                     <div className="text-center md:text-left">
-                                        <h2 className="mb-3 text-3xl sm:text-4xl font-bold text-gray-800">
+                                        <h2 className="mt-10 mb-3 text-3xl sm:text-4xl font-bold text-gray-800">
                                             Selamat Datang
                                         </h2>
                                         <p className="mb-6 sm:mb-8 text-gray-600">
@@ -222,7 +222,7 @@ export default function Login({ status, canResetPassword }) {
                                             <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l3.66-2.84z" />
                                             <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" />
                                         </svg>
-                                        <span>Login dengan Google</span>
+                                        <span>Login Google</span>
                                     </a>
 
                                     {/*  TAUTAN REGISTRASI */}
